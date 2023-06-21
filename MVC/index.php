dc3ed1e9594ee8ebb6916d7f4ef589cdd3b2da51
Diego Baltazar <institute.repositorytwo666@gmail.com>
2023-06-21 07:23:43 -0400

    Mi primer uso del gestor de paquetes composer para MVC

---
+++ MVC/index.php
@@ -0,0 +1,20 @@
+<?php
+// invocar dependencias por el gestor de paquetes
+require_once 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); // Cargar el archivo .env desde el directorio actual
+$dotenv->load(); 
+
+// Acceder a las variables de entorno
+$dbHost = $_ENV['DB_HOST'];
+$dbUser = $_ENV['DB_USER'];
+$dbPassword = $_ENV['DB_PASSWORD'];
+$dbName = $_ENV['DB_NAME'];
+
+$servername = $dbHost;
+$username = $dbUser;
+$password = $dbPassword;
+$dbname = $dbName;
+
+?>
+
+
