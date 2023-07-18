23c53172fc38ece3e11a5ada1cc7b982aabd956f
GojanDEd <candiavargaserickabraham.icepal@gmail.com>
2023-07-17 20:48:21 -0400

    condigo de ing

--- MVC/Views/templates/UserCreateRegister.php
+++ MVC/Views/templates/UserCreateRegister.php
@@ -2,23 +2,29 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Título de la página</title>
-</head>
-<body>
-<header id="site-header" class="fixed-hide">
-	
-  <div class="wrap-inner">
-  <div id="site-logo">
-  </div>
-    <nav id="main-nav" class="main-nav">
-      <ul> 
-         <li class="last-li" ><a href="http:////localhost/icepalWeb1/MVC"><span>home</span></a></li>
-         <li class="last-li" ><a href="http:////localhost/icepalWeb1/MVC/Users"><span>Usuarios registrado</span></a></li>
-       </ul>
-     </nav>
-  </div>
-</header>
+  <head>
+      <title>Título de la página</title>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="Views/styles/styles.css">
+
+      <style>
+      </style>
+  </head>
+  <body>
+  <header id="site-header" class="fixed-hide">
+    
+    <div class="wrap-inner">
+    <div id="site-logo">
+    </div>
+      <nav id="main-nav" class="main-nav">
+        <ul> 
+        <li class="last-li" ><a href="http:////localhost/icepalWeb1/MVC"><span>home</span></a></li>
+          <li class="last-li" ><a href="http:////localhost/icepalWeb1/MVC/Users"><span>Usuarios registrado</span></a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
 
 <form action="/icepalWeb1/MVC/UserCreate/create" method="POST" enctype="multipart/form-data">
     <div class="form-container">
