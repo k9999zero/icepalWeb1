03b1a2dc1703f37aeee3e652f77fb43fee86ffde
GojanDed <125888295+GojanDEd@users.noreply.github.com>
2023-09-08 19:53:45 -0400

    Update Controller.php

--- MVC/Controllers/Controller.php
+++ MVC/Controllers/Controller.php
@@ -1,17 +1,27 @@
 <?php
 namespace Controllers;
-$test = __DIR__ . '/../Models/Model.php';
-
-
-
-
-
-
-
-
-
-
-
+require_once __DIR__ . '/../Models/Model.php';
+require_once 'UserController.php';
+require_once 'ProductoController.php';
+require_once 'CategoriaController.php';
+require_once 'AdornoController.php';
+require_once 'ComentariosController.php';
+require_once 'PuntuacionController.php';
+require_once 'ClienteController.php';
+class Controller
+{
+    public function redirect($route,$data = null)
+    {
+        $separator = "/";
+        $url = $route;
+        if($data!=null)
+        {                                    
+            $url = $url.$separator.$data;
+        }
+        header("Location: ".$url);
+        exit();
+    }
+}
 
 
 
