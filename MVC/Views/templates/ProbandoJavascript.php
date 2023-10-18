ace5ea738d7235226b0458f2675b25d176d4941c
Diego Baltazar <k9999zero@gmail.com>
2023-10-17 21:20:05 -0400

    div para jquery

--- MVC/Views/templates/ProbandoJavascript.php
+++ MVC/Views/templates/ProbandoJavascript.php
@@ -1,19 +1,29 @@
 <!DOCTYPE Html>
 <html>
     <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <style>
+        #miDiv {
+            background:#98bf21;
+            height:100px;
+            width:100px;
+            position:absolute;
+        }
+        </style>
     </head>
     <body>
         <h1>hola</h1>
         <input id="miInput" type="text" value="" placeholder="PROBANDO">
         <button id="miBoton"> Probando CLick! </button>
+        <div id="miDiv">
+        </div>
         <script>
             var variable1 = 12;
             var variable1 = 18;
             variable1 = 100;
             let variable2 = 13;
             variable2 = 20;
-            const variable3 = "hola mundo";    
-            variable3 = "hola mundo2";   
+            const variable3 = "hola mundo";                 
             var jhoselin = document.getElementById("miInput"); 
             var miBoton = document.getElementById("miBoton");
             var contador = 0;
